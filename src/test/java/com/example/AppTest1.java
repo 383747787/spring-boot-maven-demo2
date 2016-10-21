551c0075ac5d8ce4da44d32469bab7fcbf783bf5
Kevin Wu <kevin@viafoura.com>
2016-10-20 22:40:46 -0400

    0013:
    - hamcrest Matcher test

--- src/test/java/com/example/AppTest1.java
+++ src/test/java/com/example/AppTest1.java
@@ -9,9 +9,12 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
-import static org.junit.Assert.assertEquals;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+
+//import static org.hamcrest.Matchers.*;
 
 /**
  * Created by kevinwu on 2016-10-20.
@@ -40,6 +43,8 @@ public class AppTest1 {
         Shipwreck wreck = sc.get(1L);
 
         verify(shipwreckRepository).findOne(1L);
-        assertEquals(1L, wreck.getId().longValue());
+        //assertEquals(1L, wreck.getId().longValue());
+
+        assertThat(wreck.getId(), is(1l));
     }
 }
