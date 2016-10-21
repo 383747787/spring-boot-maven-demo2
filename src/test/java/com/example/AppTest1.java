61c7a5bcfbaafc5b696e91bf88e620f737d3e0ed
Kevin Wu <kevin@viafoura.com>
2016-10-20 22:06:33 -0400

    0012:
    - Mockito

--- src/test/java/com/example/AppTest1.java
+++ src/test/java/com/example/AppTest1.java
@@ -1,21 +1,45 @@
 package com.example;
 
-import com.example.controller.HomeController;
+import com.example.controller.ShipwreckController;
+import com.example.model.Shipwreck;
+import com.example.repository.ShipwreckRepository;
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 /**
  * Created by kevinwu on 2016-10-20.
  */
 public class AppTest1 {
 
+
+    @InjectMocks
+    private ShipwreckController sc;
+
+    @Mock
+    private ShipwreckRepository shipwreckRepository;
+
+    @Before
+    public void init() {
+        MockitoAnnotations.initMocks(this);
+    }
+
     @Test
     public void testApp() {
 
-        HomeController hc = new HomeController();
-        String result = hc.home();
+        Shipwreck sw = new Shipwreck();
+        sw.setId(1L);
+        when(shipwreckRepository.findOne(1L)).thenReturn(sw);
 
-        assertEquals(result, "Das Boot, reporting for duty!");
+        Shipwreck wreck = sc.get(1L);
+
+        verify(shipwreckRepository).findOne(1L);
+        assertEquals(1L, wreck.getId().longValue());
     }
 }
